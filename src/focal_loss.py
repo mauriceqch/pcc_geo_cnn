a0efd8f7bd06ef4e9a4600b48467ec266b843661
Maurice Quach <maurice.quach@gmail.com>
2019-01-12 23:15:04 +0100

    Fix focal loss

--- src/focal_loss.py
+++ src/focal_loss.py
@@ -8,4 +8,4 @@ def focal_loss(y_true, y_pred, gamma=2, alpha=0.9):
     pt_1 = K.clip(pt_1, 1e-3, .999)
     pt_0 = K.clip(pt_0, 1e-3, .999)
 
-    return -K.mean(alpha * K.pow(1. - pt_1, gamma) * K.log(pt_1))-K.sum((1-alpha) * K.pow( pt_0, gamma) * K.log(1. - pt_0))
+    return -K.sum(alpha * K.pow(1. - pt_1, gamma) * K.log(pt_1)) - K.sum((1-alpha) * K.pow( pt_0, gamma) * K.log(1. - pt_0))
