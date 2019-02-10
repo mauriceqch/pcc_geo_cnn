a0a0c7377186d4ddb6cab9a88219df3409547b15
Maurice Quach <maurice.quach@gmail.com>
2019-02-10 23:14:32 +0100

    Add bdbr, tweak plots

--- src/plot_results.py
+++ src/plot_results.py
@@ -18,11 +18,12 @@ import itertools
 from matplotlib import rcParams
 import matplotlib.pyplot as plt
 import matplotlib as mpl
+import metrics
 
-rcParams['axes.labelsize'] = 12
-rcParams['xtick.labelsize'] = 12
-rcParams['ytick.labelsize'] = 12
-rcParams['legend.fontsize'] = 12
+rcParams['axes.labelsize'] = 20
+rcParams['xtick.labelsize'] = 20
+rcParams['ytick.labelsize'] = 20
+rcParams['legend.fontsize'] = 20
 rcParams['font.family'] = 'serif'
 rcParams['font.serif'] = ['Computer Modern Roman']
 rcParams['text.usetex'] = True
@@ -64,9 +65,12 @@ if __name__ == '__main__':
         fig, ax = plt.subplots()
         rd_points = [x.iloc[m][['csv_file', 'bpov', 'Symmetric,rmsFPSNR,p2plane']] for x, m in zip(input_pd, masks)]
         points_per_csv = [x.groupby('csv_file').mean() for x in rd_points]
+        points_for_bdrate = [[tuple(y) for y in x.values] for x in points_per_csv]
+        bdrate = metrics.bdrate(points_for_bdrate[1], points_for_bdrate[0])
+        logger.info(f'bdrate {s} : {bdrate}')
         for points, title, ls in zip(points_per_csv, args.titles, line_styles):
             points.plot(x='bpov', y='Symmetric,rmsFPSNR,p2plane', ax=ax, label=title, style=f'.{ls}')
-            ax.set(xlabel='bits per occupied voxel', ylabel='point to plane RMS PSNR (dB)')
+            ax.set(xlabel='bits per occupied voxel', ylabel='PSNR (dB)')
             ax.set_xlim([0, 2])
             ax.set_ylim([0, 35])
             ax.legend(loc='lower right')
@@ -75,3 +79,20 @@ if __name__ == '__main__':
         fig.tight_layout()
         fig.savefig(f'{args.output}_{s}.eps')
 
+    # BDRATE computation
+    p1 = input_pd[0].groupby('csv_file').mean()[['bpov', 'Symmetric,rmsFPSNR,p2plane']].values
+    p1 = [tuple(x) for x in p1]
+    p2 = input_pd[1].groupby('csv_file').mean()[['bpov', 'Symmetric,rmsFPSNR,p2plane']].values
+    p2 = [tuple(x) for x in p2]
+    bdrate = metrics.bdrate(p2, p1)
+    logger.info(f'bdrate: {bdrate}')
+    fig, ax = plt.subplots()
+    ax.plot([x[0] for x in p1], [x[1] for x in p1], label='Proposed', linestyle='--')
+    ax.plot([x[0] for x in p2], [x[1] for x in p2], label='Anchor', linestyle='-.')
+    ax.set(xlabel='bits per occupied voxel', ylabel='PSNR (dB)')
+    ax.set_xlim([0, 2])
+    ax.set_ylim([0, 35])
+    ax.legend(loc='lower right')
+    ax.locator_params(axis='x', nbins=5)
+    fig.tight_layout()
+    fig.savefig(f'{args.output}_overall.eps')
